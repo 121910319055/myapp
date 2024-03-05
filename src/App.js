314798ca0cc4c1788689935f24a93ad9c3436b9b
krishnakarthik <krishnakarthik@lyrostech.com>
2024-03-05 10:05:32 +0530

    insisuial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+import Header from  './Header';
+import Box1 from './Box1';
+
+function App() {
+  return (
+    <>
+    <div ><Header/> </div><hr></hr>
+    <div class="b1">Box1</div>
+
+    </>
+     
+  );
+}
+
+export default App;

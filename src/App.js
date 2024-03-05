6a17054af69f3951429dd6548a179c3f0d996bff
krishnakarthik <krishnakarthik@lyrostech.com>
2024-03-05 10:06:52 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-
-import './App.css';
-
-import Header from  './Header';
-import Box1 from './Box1';
-
+import "./App.css";
+import Calc from "./Calc";
+import Add from "./Add";
+import Students from "./Students";
+import Sqr from "./Sqr";
+import User from "./User";
 function App() {
+  const sqrfunction = (n) => {
+    return n*n
+  }
   return (
-    <>
-    <div ><Header/> </div><hr></hr>
-    <div class="b1">Box1</div>
-
-    </>
-     
+    <div>
+      <User name="John"/>
+     <Calc a={3} b={5}/>
+     <Add arr={[5,6,8]} />
+     <Students score={{'John':40,'Cathy':50}}/>
+     <Sqr a={5} fn={sqrfunction}/>
+    </div>
   );
 }
-
 export default App;

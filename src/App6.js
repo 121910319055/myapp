f4b92ebf5394d7df01cf00471205e21317bda3a8
krishnakarthik <krishnakarthik@lyrostech.com>
2024-03-07 15:55:43 +0530

    changes

--- src/App6.js
+++ src/App6.js
@@ -42,29 +42,29 @@ export default function App6() {
 
 
 /////trainer code
-import React from "react";
-import { useState, useEffect } from "react";
+// import React from "react";
+// import { useState, useEffect } from "react";
 
-export default function App6() {
-  const [runs, setRuns] = useState(0);
-  const [wickets, setWickets] = useState(0);
-  // console.log("Component loaded");
-  useEffect(() => {
-    if (wickets > 0) console.log(`Wickets:${wickets}.Better Luck Next Time!`);
-  }, [wickets]);
+// export default function App6() {
+//   const [runs, setRuns] = useState(0);
+//   const [wickets, setWickets] = useState(0);
+//   // console.log("Component loaded");
+//   useEffect(() => {
+//     if (wickets > 0) console.log(`Wickets:${wickets}.Better Luck Next Time!`);
+//   }, [wickets]);
 
-  useEffect(() => {
-    if (runs > 0) console.log(`Good Job! Score:${runs}`);
-  }, [runs]);
+//   useEffect(() => {
+//     if (runs > 0) console.log(`Good Job! Score:${runs}`);
+//   }, [runs]);
 
-  return (
-    <>
-      <button onClick={() => setRuns((prevState) => prevState + 1)}>
-        Runs({runs})
-      </button>
-      <button onClick={() => setWickets((prevState) => prevState + 1)}>
-        Wickets({wickets})
-      </button>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <button onClick={() => setRuns((prevState) => prevState + 1)}>
+//         Runs({runs})
+//       </button>
+//       <button onClick={() => setWickets((prevState) => prevState + 1)}>
+//         Wickets({wickets})
+//       </button>
+//     </>
+//   );
+// }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import App1 from './App1';
-import App6 from './App6';
+//import App1 from './App1';
+//import App6 from './App6';
+//import App8 from './App8';
+//import App9 from './App9';
+import App11 from './App11';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  // <React.StrictMode>
-    <App6 />
+    <App11 />
  // </React.StrictMode>
 );

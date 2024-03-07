d8a306449e27691f0b97bb4bc446c722f17e730f
krishnakarthik <krishnakarthik@lyrostech.com>
2024-03-07 17:21:51 +0530

    changed

--- src/App11.js
+++ src/App11.js
@@ -93,66 +93,66 @@ import { useState, useEffect } from "react";
 
 ///////
 
-// export default function App6() {
-//     const [student, setStudent] = useState({ name: "", age: "" });
-//     const [students, setStudents] = useState([]);
-//     const addStudent = () => {
-//       setStudents((prevStudents) => [...prevStudents, student]);
-//     };
-//     const deleteStudent = (name) => {
-//       setStudents(students.filter((e) => e.name !== name));
-//     };
-//     const editStudent = (value) => {
-//       setStudent(value);
-//       deleteStudent(value.name);
-//     };
-//     return (
-//       <>
-//         <p>
-//           <input
-//             type="text"
-//             value={student.name}
-//             onChange={(e) =>
-//               setStudent((prevStudent) => ({
-//                 ...prevStudent,
-//                 ...{ name: e.target.value },
-//               }))
-//             }
-//             placeholder="Enter Name"
-//           ></input>
-//         </p>
-//         <p>
-//           <input
-//             type="text"
-//             value={student.age}
-//             onChange={(e) =>
-//               setStudent((prevStudent) => ({
-//                 ...prevStudent,
-//                 ...{ age: e.target.value },
-//               }))
-//             }
-//             placeholder="Enter Age"
-//           ></input>
-//         </p>
-//         <p>
-//           <button onClick={addStudent}>Add Student</button>
-//         </p>
-//         <div>
-//           {students &&
-//             students.map((value, index) => (
-//               <div key={index}>
-//                 {value.name}-{value.age} -{" "}
-//                 <button onClick={() => deleteStudent(value.name)}>Delete</button>
-//                 <button onClick={() => editStudent(value)}>Edit</button>
-//               </div>
-//             ))}
-//         </div>
-//       </>
-//     );
-//   }
+export default function App11() {
+    const [student, setStudent] = useState({ name: "", age: "" });
+    const [students, setStudents] = useState([]);
+    const addStudent = () => {
+      setStudents((prevStudents) => [...prevStudents, student]);
+    };
+    const deleteStudent = (name) => {
+      setStudents(students.filter((e) => e.name !== name));
+    };
+    const editStudent = (value) => {
+      setStudent(value);
+      deleteStudent(value.name);
+    };
+    return (
+      <>
+        <p>
+          <input
+            type="text"
+            value={student.name}
+            onChange={(e) =>
+              setStudent((prevStudent) => ({
+                ...prevStudent,
+                ...{ name: e.target.value },
+              }))
+            }
+            placeholder="Enter Name"
+          ></input>
+        </p>
+        <p>
+          <input
+            type="text"
+            value={student.age}
+            onChange={(e) =>
+              setStudent((prevStudent) => ({
+                ...prevStudent,
+                ...{ age: e.target.value },
+              }))
+            }
+            placeholder="Enter Age"
+          ></input>
+        </p>
+        <p>
+          <button onClick={addStudent}>Add Student</button>
+        </p>
+        <div>
+          {students &&
+            students.map((value, index) => (
+              <div key={index}>
+                {value.name}-{value.age} -{" "}
+                <button onClick={() => deleteStudent(value.name)}>Delete</button>
+                <button onClick={() => editStudent(value)}>Edit</button>
+              </div>
+            ))}
+        </div>
+      </>
+    );
+  }
 
 /////////////
-
+///done
 // export default function App6() {
 //     const [student, setStudent] = useState({});
 //     const [students, setStudents] = useState([]);
@@ -205,8 +205,8 @@ import { useState, useEffect } from "react";
 //   }
 
 /////////////////
-
-// export default function App6() {
+////done
+// export default function App11() {
 //     const [student, setStudent] = useState({});
 //     const [students, setStudents] = useState([]);
 //     const addStudent = () => {
@@ -254,42 +254,42 @@ import { useState, useEffect } from "react";
 //   }
 
 /////////
-
-export default function App11() {
-    const [student, setStudent] = useState({ name: "John", age: 34 });
-    console.log(Date());
-    return (
-      <>
-        <p>
-          <input
-            type="text"
-            onChange={(e) =>
-              setStudent((prevState) => ({
-                ...prevState,
-                ...{ name: e.target.value },
-              }))
-            }
-            placeholder="Enter Name"
-          ></input>
-        </p>
-        <p>
-          <input
-            type="text"
-            onChange={(e) =>
-              setStudent((prevState) => ({
-                ...prevState,
-                ...{ age: e.target.value },
-              }))
-            }
-            placeholder="Enter Age"
-          ></input>
-        </p>
-        <span>
-          {student.name}-{student.age}
-        </span>
-      </>
-    );
-  }
+//done
+// export default function App11() {
+//     const [student, setStudent] = useState({ name: "John", age: 34 });
+//     console.log(Date());
+//     return (
+//       <>
+//         <p>
+//           <input
+//             type="text"
+//             onChange={(e) =>
+//               setStudent((prevState) => ({
+//                 ...prevState,
+//                 ...{ name: e.target.value },
+//               }))
+//             }
+//             placeholder="Enter Name"
+//           ></input>
+//         </p>
+//         <p>
+//           <input
+//             type="text"
+//             onChange={(e) =>
+//               setStudent((prevState) => ({
+//                 ...prevState,
+//                 ...{ age: e.target.value },
+//               }))
+//             }
+//             placeholder="Enter Age"
+//           ></input>
+//         </p>
+//         <span>
+//           {student.name}-{student.age}
+//         </span>
+//       </>
+//     );
+//   }
 
 ///////////////
 //done
